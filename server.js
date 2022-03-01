c500ef98ee54aa3d79fdbf0bee55fe1f9e01e169
ianwash <ianwash@live.unc.edu>
2022-03-01 00:45:46 -0500

    ya know

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-// Require Express.js
 const express = require('express')
 const app = express()
 
 var port = 5000
-// Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
 })
 
-// Default response for any other request
 app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
+    res.status(404).send('404! NOT FOUND')
+});
+
+app.get('/app/', (req, res) => {
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+    res.end(res.statusCode+ ' ' +res.statusMessage)
+});
+
+app.get('/app/flip/', (req, res) => {
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+});
+
+app.get('/app/flips/:number', (req, res) => {
+	//Some
+	//expressions
+	//go
+	//here
+});
+
+app.get('/app/flip/call/:bet', (req, res) => {
+	//Some
+	//expressions
+	//go
+	//here
 });
